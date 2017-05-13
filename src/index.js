1abbe5e5ec2664fc71faa96122e31fbec5cad52d
luixlacrux <luixlacrux18@gmail.com>
2017-05-12 23:44:50 -0400

    :sparkles: Add Async actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { render } from 'react-dom'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import {
+  selectSubreddit,
+  fetchPosts,
+  fetchPostsIfNeeded,
+} from './actions'
+import rootReducer from './reducers'
 
-render (
-  <h1>Redux subreddit</h1>,
-  document.getElementById('root')
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+  )
 )
+
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPosts('reactjs')).then(() => {
+  console.log(store.getState())
+  console.log('--- fetch posts if needed ---')
+  store.dispatch(fetchPostsIfNeeded('reactjs'))
+    .then(() => console.log(store.getState()))
+})
