f047133f43b4b00ab4de40afa4e9d4babc3548ac
luixlacrux <luixlacrux18@gmail.com>
2017-05-14 02:03:07 -0400

    :sparkles: Complete advanced tutorial

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
-// import AsycnApp from './AsyncApp'
+import AsycnApp from './AsyncApp'
 
 const store = configureStore()
 
@@ -9,7 +9,7 @@ class Root extends Component {
   render () {
     return (
       <Provider store={store}>
-        <h1>Hello</h1>
+        <AsycnApp />
       </Provider>
     )
   }
